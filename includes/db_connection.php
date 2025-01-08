f62cff36a8eda60446b5ed9b13c5da48650a0930
shyirou <roidcoki@gmail.com>
2025-01-08 21:15:46 +0700

    change

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,22 +1,19 @@
 <?php
-// Mendapatkan nilai dari environment variables
-$host = 'ep-tiny-wildflower-a1zcicy3.ap-southeast-1.aws.neon.tech';
+// Konfigurasi database
+$host = 'db.ivptfdcevuwebxwkxlud.supabase.co';
 $port = '5432';
-$dbname = 'porfolio';
-$user = 'porfolio_owner';
-$password = 'ds5efRBEOQD1';
-$sslmode = 'require';
+$dbname = 'postgres';
+$user = 'postgres';
+$password = 'R4GlROXheGBZN7Ef';
 
 try {
-    // Membuat string koneksi PDO
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=$sslmode";
-    // Membuat koneksi PDO ke database PostgreSQL
-    $pdo = new PDO($dsn, $user, $password);
-    // Menetapkan mode error PDO sebagai pengecualian
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'Connected to the database successfully!';
+    // Membuat string DSN
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
+    
+    // Membuat koneksi PDO
+    $conn = new PDO($dsn, $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // Menangani error jika koneksi gagal
-    echo 'Connection failed: ' . $e->getMessage();
+    echo "Koneksi gagal: " . $e->getMessage();
 }
 ?>
