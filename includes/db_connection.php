6e11d29949a36e90b26772ca97cc468c7a38efa7
shyirou <roidcoki@gmail.com>
2025-01-08 17:34:36 +0700

    change

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,35 +1,41 @@
 <?php
-$host = 'https://ivptfdcevuwebxwkxlud.supabase.co';
+$host = 'db.ivptfdcevuwebxwkxlud.supabase.co';
 $db = 'postgres';
 $user = 'postgres';
 $password = 'R4GlROXheGBZN7Ef';
 $port = '6543';
 
 try {
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $password);
+    // Membuat koneksi dengan PDO
+    $dsn = "pgsql:host=$host;dbname=$dbname";
+    $pdo = new PDO($dsn, $user, $password);
+
+    // Menyiapkan opsi PDO (untuk menangani error)
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to the database!";
+
+    echo "Koneksi ke database berhasil!";
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Koneksi gagal: " . $e->getMessage();
 }
+
 ?>
 
 
 <?php   
-// Define the server name where the database is hosted
-// $servername = "localhost";
-// Define the username for database access
-// $username = "root";
-// Define the password for the database user
-// $password = "";
-// Define the name of the database to connect to
-// $dbname = "motion_graphic";
+    // Define the server name where the database is hosted
+    // $servername = "localhost";
+    // Define the username for database access
+    // $username = "root";
+    // Define the password for the database user
+    // $password = "";
+    // Define the name of the database to connect to
+    // $dbname = "motion_graphic";
 
-// Create a new mysqli object to establish a connection to the database
-// $conn = new mysqli($servername, $username, $password, $dbname);
+    // Create a new mysqli object to establish a connection to the database
+    // $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check if the connection was successful; if not, terminate the script and display an error message
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
+    // Check if the connection was successful; if not, terminate the script and display an error message
+    // if ($conn->connect_error) {
+    //     die("Connection failed: " . $conn->connect_error);
+    // }
 ?>
