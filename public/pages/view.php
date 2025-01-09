d8b345aabbc175ee1cae55bc92474a2288e2a111
shyirou <roidcoki@gmail.com>
2025-01-09 13:16:39 +0700

    change

--- public/pages/view.php
+++ public/pages/view.php
@@ -33,7 +33,7 @@ if ($id > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/style.css">
     <title>View Project</title>
 </head> 
 <body>
